5868bc5f83e19df828ec7a400b28f88eda08207a
Meodinger Wang <54493746+Meodinger@users.noreply.github.com>
2021-10-27 15:13:54 +0800

    fixed spi issues

--- mp3spi/src/module-info.java
+++ mp3spi/src/module-info.java
@@ -10,6 +10,9 @@ module soundlibs.mp3spi {
     requires soundlibs.jlayer;
     requires soundlibs.tritonus.share;
 
+    provides javax.sound.sampled.spi.AudioFileReader with javazoom.spi.mpeg.sampled.file.MpegAudioFileReader;
+    provides javax.sound.sampled.spi.FormatConversionProvider with javazoom.spi.mpeg.sampled.convert.MpegFormatConversionProvider;
+
     exports javazoom.spi;
     exports javazoom.spi.mpeg.sampled.convert;
     exports javazoom.spi.mpeg.sampled.file;
--- vorbisspi/src/module-info.java
+++ vorbisspi/src/module-info.java
@@ -10,6 +10,9 @@ module soundlibs.vorbisspi {
     requires soundlibs.jorbis;
     requires soundlibs.tritonus.share;
 
+    provides javax.sound.sampled.spi.AudioFileReader with javazoom.spi.vorbis.sampled.file.VorbisAudioFileReader;
+    provides javax.sound.sampled.spi.FormatConversionProvider with javazoom.spi.vorbis.sampled.convert.VorbisFormatConversionProvider;
+
     exports javazoom.spi;
     exports javazoom.spi.vorbis.sampled.convert;
     exports javazoom.spi.vorbis.sampled.file;
